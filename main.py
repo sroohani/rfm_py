1971d26323e58b757aecd1dbaa693b61d975fc72
Shahram Roohani <shahram.roohani@gbtsolutions.pt>
2023-10-01 04:27:51 +0100

    Implement emdpoint for listing directory contents #2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 import json
+import os
 
 app = FastAPI()
 
@@ -11,3 +12,18 @@ with open('config/roots.json') as f:
 @app.get('/')
 def index():
     return {"roots": list(roots.keys())}
+
+@app.get('/{root}', status_code=status.HTTP_200_OK)
+def list_contents(response: Response, root: str, path: str | None = None):
+    contents = []
+    if root in list(roots.keys()):
+        full_path = os.path.join(roots[root], path if path else "")
+        if os.path.exists(full_path) and os.path.isdir(full_path):
+            contents = os.listdir(full_path)
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+
+    print(len(contents))
+    return contents
